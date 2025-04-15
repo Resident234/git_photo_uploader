deb9bc9093386dcea96fead5e5938d5b66308ad7
Resident234Photo <gsu4306@gmail.com>
2025-04-15 20:15:35 +0300

    Убрал проверку индекса, рандомный порядок выборки файлов

--- photo.sh
+++ photo.sh
@@ -3,6 +3,9 @@
 # пример вызова скрипта:  
 #bash photo.sh "/d/PHOTO"
 
+# В начале скрипта
+export LANG=en_US.UTF-8
+
 # Указываем путь к папке (первый аргумент)
 TARGET_DIR="$1"
 
@@ -36,84 +39,6 @@ if [ ! -d .git ]; then
   exit 1
 fi
 
-# Получаем список файлов, которые уже в индексе Git (в массив)
-mapfile -t indexed_files < <(git ls-files)
-echo "Файлов в индексе git: ${#indexed_files[@]}"
-
-# Проверим, что в массиве
-#for file in "${indexed_files[@]}"; do
-#    echo "files in index: $file"
-#done
-
-sleep 30
-
-: <<'COMMENT'
-BLG/DSC04482.JPG
-BLG/DSC04482.JPG
-BLG/DSC04482.JPG
-BLG/DSC04482.JPG
-BLG/DSC04482.JPG
-BLG/DSC04482.JPG
-BLG/DSC04482.JPG
-COMMENT
-
-
-# Declare an array to hold file paths
-declare -a files
-
-# Use find to locate files smaller than 50MB and iterate over them
-while IFS= read -r file; do
-    echo "Найден файл $file"
-    files+=("$file")
-done < <(find . -type f -size -50M -not -path "./.git/*")
-
-
-: <<'COMMENT'
-./BLG/DSC04482.JPG
-./BLG/DSC04482.JPG
-./BLG/DSC04482.JPG
-./BLG/DSC04482.JPG
-./BLG/DSC04482.JPG
-./BLG/DSC04482.JPG
-./BLG/DSC04482.JPG
-COMMENT
-
-# Iterate over the array and remove the "./" prefix
-for i in "${!files[@]}"; do
-    echo "Удаление ./ из $i"
-    files[$i]="${files[$i]#./}"
-done
-
-#for file in "${files[@]}"; do
-#    echo "files all: $file"
-#done
-
-# Array to hold the difference
-diff_files=()
-
-# Find elements in 'files' that are not in 'indexed_files'
-for file in "${files[@]}"; do
-    echo "Поиск файла $file в индексе"
-    if [[ ! " ${indexed_files[@]} " =~ " ${file} " ]]; then
-        diff_files+=("$file")
-    fi
-done
-
-# Print the difference
-#echo "Files not indexed:"
-#for diff in "${diff_files[@]}"; do
-#    echo "diff $diff"
-#done
-
-# Add "./" prefix to each element in diff_files
-for i in "${!diff_files[@]}"; do
-    echo "Добавление ./ к $i"
-    diff_files[$i]="./${diff_files[$i]}"
-done
-
-# Overwrite files with diff_files
-files=("${diff_files[@]}")
-
 # Check if .git/index.lock exists and remove it
 if [ -f ".git/index.lock" ]; then
     rm ".git/index.lock"
@@ -121,23 +46,27 @@ if [ -f ".git/index.lock" ]; then
 fi
 
 
+# Перебор всех элементов массива untracked_files
+while IFS= read -r file; do
 
-# Проходимся по массиву
-for file in "${files[@]}"; do
+    echo "Обработка файла: $file"
+    # Здесь можно выполнять какие-то действия с каждым файлом
+    # Например, проверка размера, копирование, удаление и т.д.
 
-  # Добавляем файл в Git
-  git add "$file"
+    # Добавляем файл в Git
+    git add "$file"
 
-  # Получаем имя папки, в которой находится файл
-  folder_name=$(basename "$(dirname "$file")")
+    # Получаем имя папки, в которой находится файл
+    folder_name=$(basename "$(dirname "$file")")
 
-  # Создаем коммит с именем папки
-  git commit -m "$folder_name"
+    # Создаем коммит с именем папки
+    git commit -m "$folder_name"
 
-  # Отправляем изменения в удалённый репозиторий (в основную ветку)
-  git push origin master  # todo ветку в параметр
+    # Отправляем изменения в удалённый репозиторий (в основную ветку)
+    git push origin master  # todo ветку в параметр
 
-done
+done < <(find . -type f -size -50M -not -path "./.git/*" -not -path "./.idea/*" | shuf)
+
 
 sleep 30
 echo "✅ Все файлы (меньше 50МБ) добавлены, закоммичены и отправлены в репозиторий!"
